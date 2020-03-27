7dbfc894a53c288e769018a98c66ae2705044dc0
Raymond <KarlMetz926@gmail.com>
2020-03-26 21:51:40 -0500

    GitIngore added misc changes for testing

--- src/main/java/com/chainlinkproto/controller/DashboardController.java
+++ src/main/java/com/chainlinkproto/controller/DashboardController.java
@@ -17,7 +17,7 @@ public class DashboardController
 {
 	@GetMapping("/dashboard")
 	public String getDashboard(Model model, HttpSession session) throws IOException {
-		Web3j web3 = Web3j.build(new HttpService("https://rinkeby.infura.io/v3/054531499f91402fb4093e81f66984fa"));
+		Web3j web3 = Web3j.build(new HttpService("HTTP://127.0.0.1:7545"));
 		Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().send();
 		System.out.println(web3ClientVersion.getWeb3ClientVersion());
 		return "dashboard";
